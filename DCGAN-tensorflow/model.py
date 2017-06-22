057022e083e6755415dd3480db270102ad57c526
sara <saraj.no@gmail.com>
2017-06-22 16:21:20 +0200

    fixed the issue. not the model can train. the similarity scores are negative now :-|

--- DCGAN-tensorflow/model.py
+++ DCGAN-tensorflow/model.py
@@ -151,7 +151,7 @@ class DCGAN(object):
         return tf.nn.sigmoid_cross_entropy_with_logits(logits=x, targets=y)
   
     self.d_loss_real = tf.reduce_mean(
-      sigmoid_cross_entropy_with_logits(self.D_logits_, tf.ones_like(self.D)))
+      sigmoid_cross_entropy_with_logits(self.D_logits, tf.ones_like(self.D)))
     self.d_loss_fake = tf.reduce_mean(
       sigmoid_cross_entropy_with_logits(self.D_logits_, tf.zeros_like(self.D_)))
     self.g_loss = tf.reduce_mean(
@@ -274,8 +274,6 @@ class DCGAN(object):
           batch_images = self.data_X[idx*config.batch_size:(idx+1)*config.batch_size]
           batch_labels = self.data_y[idx*config.batch_size:(idx+1)*config.batch_size]
         else:
-          batch_scores = self.all_scores[idx*config.batch_size:(idx+1)*config.batch_size].astype(np.float32)
-          print("batch_scores", batch_scores, type(batch_scores), type(batch_scores[0]))
           batch_files = self.data[idx*config.batch_size:(idx+1)*config.batch_size]
           batch = [
               get_image(batch_file,
@@ -290,6 +288,8 @@ class DCGAN(object):
           else:
             batch_images = np.array(batch).astype(np.float32)
 
+        batch_scores = self.all_scores[idx*config.batch_size:(idx+1)*config.batch_size].astype(np.float32)
+#        print("batch_scores", np.shape(batch_scores), type(batch_scores), type(batch_scores[0]))
         batch_z = np.random.uniform(-1, 1, [config.batch_size, self.z_dim]) \
               .astype(np.float32)
 
@@ -340,17 +340,17 @@ class DCGAN(object):
 
           # Update G network
           _, summary_str = self.sess.run([g_optim, self.g_sum],
-            feed_dict={ self.z: batch_z })
+            feed_dict={ self.z: batch_z , self.scores: batch_scores})
           self.writer.add_summary(summary_str, counter)
 
           # Run g_optim twice to make sure that d_loss does not go to zero (different from paper)
           _, summary_str = self.sess.run([g_optim, self.g_sum],
-            feed_dict={ self.z: batch_z })
+            feed_dict={ self.z: batch_z ,  self.scores: batch_scores})
           self.writer.add_summary(summary_str, counter)
           
-          errD_fake = self.d_loss_fake.eval({ self.z: batch_z })
+          errD_fake = self.d_loss_fake.eval({ self.z: batch_z, self.scores: batch_scores })
           errD_real = self.d_loss_real.eval({ self.inputs: batch_images, self.scores: batch_scores })
-          errG = self.g_loss.eval({self.z: batch_z})
+          errG = self.g_loss.eval({self.z: batch_z, self.scores: batch_scores })
 
         counter += 1
         print("Epoch: [%2d] [%4d/%4d] time: %4.4f, d_loss: %.8f, g_loss: %.8f" \
